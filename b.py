e5212175c0e95eb98e4086866d337896c6a51fc4
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 21:26:40 +0500

    1.7

--- b.py
+++ b.py
@@ -1,8 +1,7 @@
-#1.6
-bool(False)
-bool(None)
-bool(0)
-bool("")
-bool(())
-bool([])
-bool({})
+#1.7
+class myclass():
+    def __len__(self):
+        return 0
+
+myobj = myclass()
+print(bool(myobj))
