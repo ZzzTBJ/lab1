5470069872d10062d04c6d264f10fcdbd60ee5d6
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:20:32 +0500

    UpdateTuples, 4.17

--- b.py
+++ b.py
@@ -1,7 +1,6 @@
-#4.16
-x = ("apple", "banana", "cherry")
-y = list(x)
-y[1] = "kiwi"
-x = tuple(y)
-
-print(x)
+#4.17
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
