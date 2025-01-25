b901984ddc156221f24822cdd4c374ee35f13255
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:54:34 +0500

    PythonDict, 6.1

--- b.py
+++ b.py
@@ -1,14 +1,9 @@
-#5.20
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
-
-set3 = set1 - set2
-print(set3)
-set1.difference_update(set2)
-print(set1)
-set3 = set1.symmetric_difference(set2)
-print(set3)
-set3 = set1 ^ set2
-print(set3)
-set1.symmetric_difference_update(set2)
-print(set1)
+#6.1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(thisdict["brand"])
+print(type(thisdict))
