24ecda4bb95c366c7e4f0a0078fd0de85f87b97d
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:52:14 +0500

    JoinSets, 5.21

--- b.py
+++ b.py
@@ -2,6 +2,13 @@
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
 
-set3 = set1.difference(set2)
-
+set3 = set1 - set2
 print(set3)
+set1.difference_update(set2)
+print(set1)
+set3 = set1.symmetric_difference(set2)
+print(set3)
+set3 = set1 ^ set2
+print(set3)
+set1.symmetric_difference_update(set2)
+print(set1)
