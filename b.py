f95b04da243ba9c00bd8d6f04644a40999b2c303
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-26 00:14:07 +0500

    IfElse, 7.3

--- b.py
+++ b.py
@@ -1,11 +1,13 @@
 #7.1
-#Ternary operators
-a = 2
-b = 330
-print("A") if a > b else print("B")
+# keywords:
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
 
+if a > b or a > c:
+  print("At least one of the conditions is True")
 
-
-a = 330
-b = 330
-print("A") if a > b else print("=") if a == b else print("B")
+if not a > b:
+  print("a is NOT greater than b")
