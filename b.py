d5b0699373c8fab166138efbd650462622a0cb7e
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:30:25 +0500

    PythonSets, 5.6

--- b.py
+++ b.py
@@ -1,4 +1,9 @@
-#5.5
-thisset = {"apple", "banana", "cherry"}
-
-print(len(thisset))
+#5.6
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(set2)
+print(set3)
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
