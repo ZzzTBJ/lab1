29770914944fc904dc27be9515514f28b97e9aef
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-26 00:11:44 +0500

    NestedDict, 6.14

--- b.py
+++ b.py
@@ -1,10 +1,23 @@
-#6.11
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+#6.14
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
 }
-mydict = thisdict.copy()
-print(mydict)
-mydict = dict(thisdict)
-print(mydict)
+
+print(myfamily)
+print(myfamily["child2"]["name"])
+for x, obj in myfamily.items():
+  print(x)
+
+  for y in obj:
+    print(y + ':', obj[y])
