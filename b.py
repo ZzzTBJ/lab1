4bc8674fd8a730eccd22c14bd92a5010acef5c31
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 21:44:11 +0500

    AssignmentOperators

--- b.py
+++ b.py
@@ -1,18 +1,28 @@
-#2.2
-x = 5
-y = 3
-print(x + y)
-print(x - y)
-print(x * y)
-x = 12
-y = 3
-print(x / y)
-x = 5
-y = 2
-print(x % y)
-x = 2
-y = 5
-print(x ** y) #same as 2*2*2*2*2
-x = 15
-y = 2
-print(x // y)#the floor division // rounds the result down to the nearest whole number
+#2.3
+x = 5 # x=5
+print(x) 
+x += 3 #x = x+3
+print(x)
+x -= 3 #x = x-3
+print(x)
+x *= 3 #x = x*3
+print(x)
+x /= 3 #x = x/3
+print(x)
+x %= 3 #x = x%3
+print(x)
+x //= 3 #x = x//3
+print(x)
+x **= 3 #x = x**3
+print(x)
+x &= 3 #x = x&3
+print(x)
+x |= 3 #x = x|3
+print(x)
+x ^= 3 #x = x^3
+print(x)
+x >>= 3 #x = x >> 3
+print(x)
+x <<= 3 #x = x << 3
+print(x)
+print(x := 3) #x = 3 print(x)
