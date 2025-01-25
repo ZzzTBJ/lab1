145d331990af2f909b47f26e33ec592bb934e6a2
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-26 00:13:49 +0500

    IfElse, 7.2

--- b.py
+++ b.py
@@ -1,13 +1,11 @@
 #7.1
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
+#Ternary operators
+a = 2
+b = 330
+print("A") if a > b else print("B")
 
 
-#One line if statement:
-if a > b: print("a is greater than b")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
