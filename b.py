9c0a610d1d3550c5aceadb95ce6faf67107cc9e5
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:30:56 +0500

    PythonSets, 5.7

--- b.py
+++ b.py
@@ -1,9 +1,5 @@
 #5.6
-set1 = {"apple", "banana", "cherry"}
-set2 = {1, 5, 7, 9, 3}
-set3 = {True, False, False}
-print(set1)
-print(set2)
-print(set3)
-set1 = {"abc", 34, True, 40, "male"}
-print(set1)
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
