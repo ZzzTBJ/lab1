574d1aad0134434131f28847e6ac25949e3fe435
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 22:53:31 +0500

    3.43

--- b.py
+++ b.py
@@ -1,7 +1,8 @@
-#3.42
+#3.43
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
-newlist = ['hello' for x in fruits]
+newlist = [x if x != "banana" else "orange" for x in fruits]
 
 print(newlist)
+
 
