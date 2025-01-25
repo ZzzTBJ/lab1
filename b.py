14244221367800ec9203f069d96cc2b7c9ff34fd
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:01:27 +0500

    3.49

--- b.py
+++ b.py
@@ -1,9 +1,6 @@
-#3.48
-def myfunc(n):
-  return abs(n - 50)
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort(key = myfunc)
+#3.49
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
 print(thislist)
 
 
