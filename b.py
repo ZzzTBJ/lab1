de799b8c2874213bf9ba23745d690eec62d766e3
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 21:49:59 +0500

    IdentityOperators

--- b.py
+++ b.py
@@ -1,5 +1,11 @@
-#2.5
-x = 5
-print(x > 3 and  x < 10)
-print(x > 3 or x < 4)
-print(not(x < 3 and x < 10))
+#2.6
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z)
+print(x is y)
+print(x == y)
+
+print(x is not z)
+print(x is not y)
+print(x != y)
