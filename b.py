c4ebbc781db9bc576b7dc8cc6d4bfa9fda2fb7b9
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 21:47:19 +0500

    LogicalOperators

--- b.py
+++ b.py
@@ -1,9 +1,5 @@
-#2.4
+#2.5
 x = 5
-y = 3
-print( x == y )
-print( x != y )
-print( x > y )
-print( x < y )
-print( x >= y )
-print( x <= y )
+print(x > 3 and  x < 10)
+print(x > 3 or x < 4)
+print(not(x < 3 and x < 10))
