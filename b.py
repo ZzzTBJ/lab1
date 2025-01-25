fc457375d79476e26ae080b596741576f0961fb3
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 21:50:59 +0500

    MembershipOperators

--- b.py
+++ b.py
@@ -1,11 +1,4 @@
-#2.6
+#2.7
 x = ["apple", "banana"]
-y = ["apple", "banana"]
-z = x
-print(x is z)
-print(x is y)
-print(x == y)
-
-print(x is not z)
-print(x is not y)
-print(x != y)
+print("banana" in x)
+print("pineapple" not in x)
