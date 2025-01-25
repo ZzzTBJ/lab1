8ff8eab6979877fcce50b46dd06e976098ffee85
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-26 00:14:22 +0500

    IfElse, 7.4

--- b.py
+++ b.py
@@ -1,13 +1,10 @@
 #7.1
-# keywords:
-a = 200
-b = 33
-c = 500
-if a > b and c > a:
-  print("Both conditions are True")
+# Nested If
+x = 41
 
-if a > b or a > c:
-  print("At least one of the conditions is True")
-
-if not a > b:
-  print("a is NOT greater than b")
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
