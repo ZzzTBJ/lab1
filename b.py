caaa17eb3feb533df7b0879e974ab37551fa8571
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:31:59 +0500

    AccessSetItems, 5.8

--- b.py
+++ b.py
@@ -1,5 +1,5 @@
-#5.6
-myset = {"apple", "banana", "cherry"}
-print(type(myset))
-thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
-print(thisset)
+#5.8
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
