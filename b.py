22bab8377ed884d7ba19a1277b05862f2e4033d6
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:11:37 +0500

    PythonTuples, 4.5

--- b.py
+++ b.py
@@ -1,8 +1,4 @@
-#4.4
-thistuple = ("apple",)
-print(type(thistuple))
-
-#NOT a tuple
-thistuple = ("apple")
-print(type(thistuple))
-
+#4.5
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
