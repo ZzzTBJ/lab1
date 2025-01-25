a63a75c5da98bce7be0a72ba3d928475300e3438
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-26 00:10:19 +0500

    CopyDicts, 6.13

--- b.py
+++ b.py
@@ -4,13 +4,7 @@ thisdict = {
   "model": "Mustang",
   "year": 1964
 }
-for x in thisdict:
-  print(x)
-for x in thisdict:
-  print(thisdict[x])
-for x in thisdict.values():
-  print(x)
-for x in thisdict.keys():
-  print(x)
-for x, y in thisdict.items():
-  print(x, y)
+mydict = thisdict.copy()
+print(mydict)
+mydict = dict(thisdict)
+print(mydict)
