6505d8020b0738ebe34ef41f89047528777a1b00
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 22:49:04 +0500

    3.36

--- b.py
+++ b.py
@@ -1,9 +1,6 @@
-#3.35
+#3.36
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
 
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
+newlist = [x for x in fruits if "a" in x]
 
 print(newlist)
