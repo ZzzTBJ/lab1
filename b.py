f464698dc1af251e34617ae7762f28f0c88f172e
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-25 23:19:57 +0500

    UpdateTuples, 4.16

--- b.py
+++ b.py
@@ -1,4 +1,7 @@
-#4.15
-thistuple = ("apple", "banana", "cherry")
-if "apple" in thistuple:
-  print("Yes, 'apple' is in the fruits tuple")
+#4.16
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
