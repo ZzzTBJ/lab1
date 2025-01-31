a3b4e26fe5b0d68ee9a27147dbec4ed274b1e8c6
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-31 21:34:12 +0500

    Classes 3.2

--- lab3.py
+++ lab3.py
@@ -1,5 +1,7 @@
-class MyClass:
-    x = 5
-
-p1 = MyClass()
-print(p1.x)
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+p1 = Person("John", 35)
+print(p1.name)
+print(p1.age)
