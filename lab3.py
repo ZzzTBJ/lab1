4747a5770eefae7965eb9212609a6ad899e812d0
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-31 21:37:29 +0500

    Classes 3.3

--- lab3.py
+++ lab3.py
@@ -2,6 +2,10 @@ class Person():
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def __str__(self):
+        return f"This person is {self.name} and is {self.age} years old"
+
+
 p1 = Person("John", 35)
-print(p1.name)
-print(p1.age)
+print(p1)
