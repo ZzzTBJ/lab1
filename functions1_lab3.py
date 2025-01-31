e0c318d7dfdd73b45bcf620c8254afbf71b9bf8e
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-31 22:22:17 +0500

    third

--- functions1_lab3.py
+++ functions1_lab3.py
@@ -1,3 +1,5 @@
-def Fahrenheit(degree):
-    print((5/9) * (degree-32))
-Fahrenheit(int(input("How many degrees in Fahrenheit: ")))
+def Solve(heads, legs):
+    chicken = -(legs - 4*heads)/2
+    rabb = heads - chicken
+    return rabb, chicken
+print(Solve(35,94))
