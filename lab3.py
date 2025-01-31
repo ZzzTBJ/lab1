f0863170d162ebca0d70e39e5e6f12ab1445c5db
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-31 21:28:33 +0500

    Lambda 2.2

--- lab3.py
+++ lab3.py
@@ -1,8 +1,8 @@
-x = lambda a : a + 10
-print(x(5))
+def myfunc(n):
+  return lambda a : a * n
 
-x = lambda a, b : a * b
-print(x(5, 6))
+mydoubler = myfunc(2)
+print(mydoubler(11))
 
-x = lambda a, b, c : a + b + c
-print(x(5, 6, 2))
+mytripler = myfunc(3)
+print(mytripler(11))
