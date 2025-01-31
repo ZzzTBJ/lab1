7b81240341bfceb65f834f67f2c3578712d41b9f
ZhantuovaZ <oldfagprvt@gmail.com>
2025-01-31 23:08:08 +0500

    Fifth

--- functions1_lab3.py
+++ functions1_lab3.py
@@ -1,15 +1,6 @@
-def Filter(list):
-    new = []
-    for num in list:
-        if num < 1:
-            continue
-        bool = True
-        for j in range(2, int(pow(num, 1/2))+1):
-            if num%j == 0:
-                bool = False
-                break
-        if bool:
-            new.append(num)
-    print(new)
-arr = list(map(int, input().split()))
-Filter(arr)
+from itertools import permutations
+def Perm(str):
+    return list(permutations(str))
+str = Perm('abc')
+for letter in str:
+    print(letter)
