380918e67ab0d31088742d805eaea44bd1af2044
ZhantuovaZ <oldfagprvt@gmail.com>
2025-02-01 20:15:23 +0500

    seventh

--- functions1_lab3.py
+++ functions1_lab3.py
@@ -1,3 +1,18 @@
-def Reverse(str = input()):
-    print(' '.join(str.split()[::-1]))
-Reverse()
+def Has_33(nums):
+    arr = []
+    cnt = 0
+    for i in nums:
+        arr.append(i)
+    for j in range(len(arr) - 1):
+        if arr[j] == 3 and arr[j+1] == 3:
+            cnt += 1
+        else:
+            cnt += 0
+    if cnt == 1:
+        print("True")
+    elif cnt == 0:
+        print("False")
+
+Has_33([1,3,3])
+Has_33([1,3,1,3])
+Has_33([3,1,3])
