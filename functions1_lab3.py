df52ee88b8f928dc579218e3eeab746e6617b820
ZhantuovaZ <oldfagprvt@gmail.com>
2025-02-02 18:38:32 +0500

    eight

--- functions1_lab3.py
+++ functions1_lab3.py
@@ -1,18 +1,14 @@
-def Has_33(nums):
-    arr = []
+def Spy_game(nums):
+    code = [0, 0, 7]
     cnt = 0
-    for i in nums:
-        arr.append(i)
-    for j in range(len(arr) - 1):
-        if arr[j] == 3 and arr[j+1] == 3:
+    
+    for num in nums:
+        if cnt < len(code) and num == code[cnt]:
             cnt += 1
-        else:
-            cnt += 0
-    if cnt == 1:
-        print("True")
-    elif cnt == 0:
-        print("False")
+        if cnt == len(code):
+            return True
+    return False
 
-Has_33([1,3,3])
-Has_33([1,3,1,3])
-Has_33([3,1,3])
+print(Spy_game([1, 2, 4, 0, 0, 7, 5]))  
+print(Spy_game([1, 0, 2, 4, 0, 5, 7]))  
+print(Spy_game([1, 7, 2, 0, 4, 5, 0]))  
