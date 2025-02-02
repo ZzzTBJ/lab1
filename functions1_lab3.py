2bc7dcc314d647efccccfb1c64ca3fa7463b783d
ZhantuovaZ <oldfagprvt@gmail.com>
2025-02-02 18:59:42 +0500

    12

--- functions1_lab3.py
+++ functions1_lab3.py
@@ -1,6 +1,4 @@
-def Palindrome(str):
-    if str[::-1].casefold() == str.casefold():
-        print("It is palindrome")
-    else:
-        print("It is NOT palindrome")
-Palindrome(input())
+def Histogram(nums):
+    for i in nums:
+        print("*" * i)
+Histogram([4,9,7])
