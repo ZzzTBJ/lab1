ede549cf9336e2cec3bb76c198bec0c7e135ca11
ZhantuovaZ <oldfagprvt@gmail.com>
2025-02-02 20:06:01 +0500

    Second

--- classes_lab3.py
+++ classes_lab3.py
@@ -1,10 +1,14 @@
-class str:
+class Shape:
     def __init__(self):
-        self.string = ""
-    def getString(self):
-        self.string = input()
-    def printString(self):
-        print(self.string.upper())
-check = str()
-check.getString()
-check.printString()
+        self.area = 0
+    def Area(self):
+        print("The area of shape:", self.area)
+class Square(Shape):
+    def __init__(self, length):
+        Shape.__init__(self)
+        self.l = length
+        self.area = self.l*self.l
+x1 = Shape()
+x2 = Square(3)
+x1.Area()
+x2.Area()
