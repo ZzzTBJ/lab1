07aba7e931465798119597b8a3bf9ae2603f605f
ZhantuovaZ <oldfagprvt@gmail.com>
2025-02-02 21:14:41 +0500

    fourth

--- classes_lab3.py
+++ classes_lab3.py
@@ -1,15 +1,18 @@
-class Shape:
-    def __init__(self):
-        self.area = 0
-    def Area(self):
-        print("The area of shape:", self.area)
-class Rectangle(Shape):
-    def __init__(self, length, width):
-        Shape.__init__(self)
-        self.l = length
-        self.w = width
-        self.area = self.l*self.w
-x1 = Shape()
-x2 = Rectangle(4,3)
-x1.Area()
-x2.Area()
+import math
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def show(self):
+        print(f"(x,y) = ({self.x}, {self.y})")
+    def move(self,x2,y2):
+        self.x += x2
+        self.y += y2
+    def dist(self, x2,y2):
+        d = math.sqrt(pow(x2-self.x, 2)+pow(y2-self.y,2))
+        print("The distance:", d)
+p = Point(1,2)
+p.show()
+p.move(1, -2)
+p.show()
+p.dist(2,1)
