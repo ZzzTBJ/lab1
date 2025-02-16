05c7416c05043280bafde28b41e95533aedcc282
ZhantuovaZ <oldfagprvt@gmail.com>
2025-02-16 15:34:27 +0500

    lab4_json

--- lab4/json/lab4_4.py
+++ lab4/json/lab4_4.py
@@ -1,7 +1,12 @@
 import json
-with open("sample-data.json", 'r') as file:
+with open("c:/Users/Зухра/Desktop/PP2/lab1/lab4/json/sample-data.json", "r") as file:
     data = json.load(file)
 interf = data["imdata"]
-print("Interface Status ", end= "")
-print("="*50)
-print("DN", " "*40, "Description", " "*10, "Speed", " "*5, "MTU" )
+print("Interface Status ")
+print("="*70)
+print("DN", " "*43, "Description", " "*5, "Speed", " "*3, "MTU" )
+print("-"*43, " ", "-"*15, " ", "-"*7, " ", "-"*5)
+for dat in interf:
+    for i in dat:
+        for j in dat[i]:
+            print(dat[i][j]["dn"], "\t", "\t", "\t", dat[i][j]["speed"]," ", dat[i][j]["mtu"])
